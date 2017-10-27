a269c5e4fe0498256012072b8c53c20b6d3ca29b
dimkaBanDit <33134404+dimkaBanDit@users.noreply.github.com>
2017-10-27 17:23:28 +0400

    lol

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 var elem = document.getElementById('butt');
 elem.addEventListener('click', vverh);
-elem.addEventListener('click', bok);
 
 function vverh() {
+	this.style.marginLeft = "10px";
+	this.style.transform = "rotate(-360deg)";
+	
 	let elem = document.getElementById('golova');
 	elem.style.top = 0;
+	window.setTimeout(func, 3000);
+	window.setTimeout(span, 10000);
+
 }
 
-function bok() {
-	let elem = document.getElementById('bok');
-	elem.style.left = 0;
-	elem.style.height = "650px";
+function func() {
+	let elem = document.getElementById('butt');
+	elem.style.borderRadius = 0;
 }
+
+
+
+function span() {
+	var parent = document.getElementById('golova');
+	var span = document.createElement('span');
+	span.innerHTML = 'В Падике';
+	span.style.cssText = 'float: left; display: block; margin-top: 10px; margin-left: 10px; height: 80px; padding-left: 5px; border-left: 1px solid black; font-size: 70px; font-weight: lighter;';
+	span.classList.add('animated');
+	span.classList.add('bounceInRight');
+	parent.appendChild(span);
+}
+
+
