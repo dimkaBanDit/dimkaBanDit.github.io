d5627612ecaac425322c63579d9943307f263ca3
dimkaBanDit <33134404+dimkaBanDit@users.noreply.github.com>
2017-10-27 19:59:28 +0400

    pre alfa v0.5

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function vverh() {
 	elem.style.top = 0;
 	window.setTimeout(func, 3000);
 	window.setTimeout(span, 9000);
+	window.setTimeout(bok, 10000);
 	this.removeEventListener('click', vverh);
 
 }
@@ -21,13 +22,28 @@ function func() {
 
 
 function span() {
-	var parent = document.getElementById('golova');
-	var span = document.createElement('span');
+	let parent = document.getElementById('golova');
+	let span = document.createElement('span');
 	span.innerHTML = 'VPadike';
 	span.style.cssText = 'float: left; display: block; margin-top: 10px; margin-left: 10px; height: 80px; padding-left: 5px; border-left: 1px solid black; font-size: 70px; font-weight: lighter;';
 	span.classList.add('animated');
 	span.classList.add('bounceIn');
 	parent.appendChild(span);
 }
+
+function bok() {
+	let parent = document.body;
+	let di = document.createElement('div');
+	let di1 = document.createElement('div');
+	di.classList.add('animated');
+	di.classList.add('bounceInLeft');
+	di.classList.add('bok');
+	di1.classList.add('animated');
+	di1.classList.add('bounceInRight');
+	di1.classList.add('content');
+	parent.appendChild(di);
+	parent.appendChild(di1);
+}
+
 
 
