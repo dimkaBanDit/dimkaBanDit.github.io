3f07028e79c4f2f3890667a358002ca8d6502501
dimkaBanDit <33134404+dimkaBanDit@users.noreply.github.com>
2017-10-28 20:52:48 +0400

    pre alfa v0.6

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ function vverh() {
 	window.setTimeout(span, 6000);
 	window.setTimeout(bok, 7000);
 	window.setTimeout(menu, 8000);
+	window.setTimeout(menuic, 8000);
 	this.removeEventListener('click', vverh);
 
 }
@@ -30,7 +31,7 @@ function span() {
 	span.innerHTML = 'VPadike';
 	span.style.cssText = 'float: left; display: block; margin-top: 10px; margin-left: 10px; height: 80px; padding-left: 5px; border-left: 1px solid black; font-size: 70px; font-weight: lighter;';
 	span.classList.add('animated');
-	span.classList.add('bounceIn');
+	span.classList.add('zoomIn');
 	parent.appendChild(span);
 }
 
@@ -50,10 +51,25 @@ function bok() {
 
 function menu() {
 	let elems = document.getElementsByTagName('div');
+		for (var i = 0; i < 8; i++) {
 		button = document.createElement('button');
 		button.classList.add('button');
 		button.classList.add('animated');
 		button.classList.add('bounceIn');
 		elems[1].appendChild(button);
+		}
 }
+
+function menuic() {
+	let elems = document.getElementsByTagName('button');
+		 	elems[1].innerHTML = '<i class="fa fa-envelope" aria-hidden="true"></i>';
+		 	elems[2].innerHTML = '<i class="fa fa-camera-retro" aria-hidden="true"></i>';
+		 	elems[3].innerHTML = '<i class="fa fa-home" aria-hidden="true"></i>';
+		 	elems[4].innerHTML = '<i class="fa fa-comments" aria-hidden="true"></i>';	
+		 	elems[5].innerHTML = '<i class="fa fa-group" aria-hidden="true"></i>';
+		 	elems[6].innerHTML = '<i class="fa fa-newspaper-o" aria-hidden="true"></i>';
+		 	elems[7].innerHTML = '<i class="fa fa-film" aria-hidden="true"></i>';
+		 	elems[8].innerHTML = '<i class="fa fa-play-circle" aria-hidden="true"></i>';
+}
+
 
