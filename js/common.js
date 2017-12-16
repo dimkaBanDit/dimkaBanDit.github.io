ee297bca7bb22e11cc2e1d72f3c8cc325f542628
dimkaBanDit <33134404+dimkaBanDit@users.noreply.github.com>
2017-12-17 00:05:08 +0400

    alfa 1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 $('.vp').click(function() {
 	$(this).hide();
 	$('.bor').animate({height: '400px', marginTop: '20%'}, 1000, function() {
@@ -19,10 +20,6 @@ $('.vp').click(function() {
 		})
 	});
 });
-
-
-
-
 function start() {
 	$('#head').after("<div id='he' class='container'></div>");
 	$('#he').append("<div id='he1' class='row justify-content-md-center'></div>");
@@ -31,5 +28,24 @@ function start() {
 	$('.bor').animate({'margin-top': '5px'}, 1000, function() {
 		$('.vp').css({transform: "rotate(-360deg)",marginLeft: '-5px'}).animate({ borderRadius: '10px'}, 1000);
 		$('#he1').append("<div id='menu' class='col-lg-3 fon animated zoomInRight'></div>");
-		$('#he1').append("<div id='content' class='col-lg-7 fon animated zoomInLeft'></div>");
+		$('#he1').append('<div id="content" class="col-lg-7 fon animated zoomInLeft"><div class="contenter"><div class="msg"></div><div class="niz"><input type="text" id="ms"><button id="ent">Enter</button></div></div></div>');
 	});};
+
+		function selfRandom(min, max)
+		{
+  			return Math.floor(Math.random() * (max - min + 1)) + min;
+		};
+	$('body').on('click', '#ent', function () {
+		var msg = $('#ms').val();
+		var kek = ['Doroy','Privet','Hellow','Привет','Дратути','Дороу'];
+		$('.msg').append('<div class="row"><div class="mas"><p class="lol">'+msg+'</p></div></div>');
+		for (var i = 0; i < kek.length; i++) {
+		if (msg == kek[i]) {
+			$('.msg').append('<div class="row"><div class="mos"><p class="lol">'+kek[selfRandom(0,kek.length - 1)]+'</p></div></div>');
+			$('#ms').val('');
+		    }
+		}	
+	});	
+})
+
+
