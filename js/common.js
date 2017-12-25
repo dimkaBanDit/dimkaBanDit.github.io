926c62f2272b0a1291e09d5fdd6914c5fc3b1627
dimkaBanDit <33134404+dimkaBanDit@users.noreply.github.com>
2017-12-25 18:44:02 +0400

    Vpadile v3.1

--- js/common.js
+++ js/common.js
@@ -33,7 +33,8 @@ $(document).ready(function () {
 		if (kek == 'Home') { Home(kek); } if (kek == 'Photo') { Photo(kek); } if (kek == 'Messege') { Messege(kek); } if (kek == 'News') { News(kek); }if (kek == 'People') { People(kek); }if (kek == 'Video') { Video(kek); };
 	};
 	function Home(cl) {
-		$('.container').append('<div class="info"></div>');
+		$('.container').append('<div class="info"><img class="ava" src="img/dimka.jpg" alt="" /><div class="inf"><span class="levo name">Dimka BanDit</span><span class="levo">Onlaine Forever</span></div></div><div class="stena"></div>');
+		$('.stena').append('<div class="soc"><p class="levo font">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, optio in eaque sint dolorum distinctio illo sequi deleniti veritatis voluptatum! Fugit deserunt, explicabo eveniet, amet id tempora fuga repellat soluta?</p></div><div class="soc"><p class="levo font">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, optio in eaque sint dolorum distinctio illo sequi deleniti veritatis voluptatum! Fugit deserunt, explicabo eveniet, amet id tempora fuga repellat soluta?</p></div>');
 	};
 	function Photo(cl) {
 		for (var i = 1; i < 11; i++) {
@@ -50,6 +51,15 @@ $(document).ready(function () {
 		$('.container').append('<span>'+cl+'</span>')
 	};
 	function Video(cl) {
-		$('.container').append('<span>'+cl+'</span>')
+		var poper = $('.container');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/zObfNvSTf7s" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/kOCxHu_F5xo" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/9Ebdpv0KOlM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/_ZMrhPrwmb4" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/7Rn1NT5dlIc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/AMiZwXoWp_Y" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/UMEChE0gn_w" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/s-C84Sm9_YI" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
+		poper.append('<div class="sosi"><iframe width="854" height="480" src="https://www.youtube.com/embed/UTbVpbxQOJw" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe></div>');
 	};
 });
